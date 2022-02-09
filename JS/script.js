87020ebc6010cb3da295a40b982516aa85246687
pablo23232 <pablo23232@gmail.com>
2022-02-09 23:54:14 +0000

    funcion para cambiar de cancion(solo para una cancion por ahora)

--- JS/script.js
+++ JS/script.js
@@ -1,12 +1,21 @@
 const DOM = {
     mp3: document.getElementById("mp3"),
     gurenge: document.getElementById("gurenge"),
-    kimetsu: document.getElementById("kimetsu")
+    kimetsu: document.getElementById("kimetsu"),
+    audio: document.getElementById("audioPlayer"),
+    imagenCancion: document.getElementById("imagen")
 };
 DOM.kimetsu.addEventListener("click", reproducirMusica);
+DOM.gurenge.addEventListener("click", reproducirMusica);
 
 function reproducirMusica(){
-    if(this.id== "kimetsu"){
-        DOM.mp3.src ="";
+    if(this.id== "kimetsu" || this.id== "gurenge" ){
+        console.log("funciona");
+        DOM.audio.pause();
+        DOM.audio.currentTime = 0;
+        DOM.mp3.src="music/gurenge.mp3";
+        DOM.imagenCancion.setAttribute("class","kimetsuCancion");
+        DOM.audio.load();
+        DOM.audio.play();
     }
 }
